5d60b03200466321bf8183db30b4d53eff5d5b24
Michael Deardeuff <grignaak@gmail.com>
2011-06-17 16:36:44 -0700

    A very simple model
    Added some helpers for tests

--- spec/money_model_spec.rb
+++ spec/money_model_spec.rb
@@ -1,4 +1,46 @@
-describe :money do
-  it "should" do
+require 'money_model'
+require 'test/unit'
+
+module Money
+module Model
+
+describe Principle do
+  it "should accept a yearly income" do
+    Principle.asset(1200, :per => :year).balance.should == 100
   end
+
+  it "should accept a monthly outflow" do
+    Principle.liability(100).balance.should == -100
+  end
+end
+
+describe Category do
+  it "should accept a sub-item" do
+    c = Category.new 
+    c << Principle.asset(0)
+    c.sub_items.length.should == 1
+  end
+
+  it "should keep a balance" do
+    c = Category.new
+    c.balance.should == 0
+  end
+
+  it "should keep a balance of it's sub-item" do
+    c = Category.new
+    c << Principle.asset(100)
+    c.balance.should == 100
+  end
+end
+
+
+describe Tax do
+  it "should take a percentage off it's argument" do
+    tax = Tax.new 10, Principle.asset(1000)
+    tax.balance.should == 100
+  end
+end
+
+
+end
 end
--- src/money_model.rb
+++ src/money_model.rb
@@ -1,3 +1,51 @@
-module Money::Model
+module Money
+module Model
 
+  class Category
+    attr_reader :sub_items
+
+    def initialize()
+      @sub_items = []
+    end
+
+    def <<(sub_category)
+      @sub_items << sub_category
+    end
+
+    def balance
+      sub_items.inject(0) do |total, category|
+        total + category.balance
+      end
+    end
+  end 
+
+  class Principle
+    def self.asset(*args)
+      new(*args)
+    end
+
+    def self.liability(balance, *rest)
+      new(-balance, *rest)
+    end
+
+    attr_reader :balance
+
+    def initialize(balance, opts = {})
+      balance /= 12 if opts[:per] == :year
+      @balance = balance
+    end
+  end
+
+  class Tax
+    def initialize(percent, principle)
+      @percent = percent
+      @principle = principle
+    end
+
+    def balance
+      @principle.balance * (@percent / 100.0)
+    end
+  end
+
+end
 end
